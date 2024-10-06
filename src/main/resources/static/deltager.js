cc388170dc1726dfbf8fa32a606fae64af87a500
Simen Strømsnes <simen@simsine.no>
2024-10-06 17:55:04 +0200

    Oppgave 2 fungerer

--- src/main/resources/static/deltager.js
+++ src/main/resources/static/deltager.js
@@ -5,6 +5,11 @@ class DeltagerManager {
 	#deltagerNavnInput;
 	#sluttTidInput;
 	#deltagerOutput;
+
+	#fraTidInput;
+	#tilTidInput;
+	#listeTableBody;
+	#listeIngenResultater;
 	
 	#deltagere = {};
 
@@ -14,8 +19,16 @@ class DeltagerManager {
 		this.#sluttTidInput = root.querySelector("#sluttid");
 		this.#deltagerOutput = root.querySelector("p.hidden");
 		
-		const registrerButton = document.querySelector("fieldset.registrering button");
+		const registrerButton = root.querySelector("fieldset.registrering button");
 		registrerButton.addEventListener("click", () => this.#registrerDeltager());
+
+		this.#fraTidInput = root.querySelector("#nedregrense");
+		this.#tilTidInput = root.querySelector("#ovregrense");
+		this.#listeTableBody = root.querySelector("div.liste tbody")
+		this.#listeIngenResultater = root.querySelector("div.liste p")
+
+		const visDeltagereButton = root.querySelector("fieldset.resultat button");
+		visDeltagereButton.addEventListener("click", () => this.#visDeltagere());
     }
 	
 	#registrerDeltager() {
@@ -71,6 +84,66 @@ class DeltagerManager {
 	        .join("");
 	}
 	
+	#visDeltagere() {
+		const fraTid = this.#fraTidInput.value;
+		const tilTid = this.#tilTidInput.value;
+		
+		if (fraTid !== "" && tilTid !== "" && fraTid > tilTid) {
+			this.#tilTidInput.setCustomValidity("Øvre grense må være større enn nedre grense");
+			this.#tilTidInput.reportValidity();
+			this.#tilTidInput.focus();
+			return;
+		}
+		this.#tilTidInput.setCustomValidity("");
+		this.#listeTableBody.innerHTML = "";
+
+		let sorterteDeltagere;
+		sorterteDeltagere = Object.entries(this.#deltagere);
+		sorterteDeltagere.sort((a, b) => a[1].sluttTid > b[1].sluttTid);
+		sorterteDeltagere.forEach((deltager, indeks) => deltager[2] = indeks+1);
+
+		if (fraTid !== "") {
+			sorterteDeltagere = sorterteDeltagere.filter(a => a[1].sluttTid >= fraTid);
+		}
+
+		if (tilTid !== "") {
+			sorterteDeltagere = sorterteDeltagere.filter(a => a[1].sluttTid <= tilTid);
+		}
+
+		if (sorterteDeltagere.length < 1) {
+			this.#listeIngenResultater.classList.remove("hidden");
+			return;
+		}
+
+		this.#listeIngenResultater.classList.add("hidden");
+	
+		sorterteDeltagere.forEach(deltager => {
+			let nyrad = document.createElement("tr");
+			let radData;
+
+			// Plassering
+			radData = document.createElement("td");
+			radData.innerText = deltager[2];
+			nyrad.appendChild(radData);
+
+			// Startnummer
+			radData = document.createElement("td");
+			radData.innerText = deltager[0];
+			nyrad.appendChild(radData);
+
+			// Navn
+			radData = document.createElement("td");
+			radData.innerText = deltager[1].deltagerNavn;
+			nyrad.appendChild(radData);
+
+			// Sluttid
+			radData = document.createElement("td");
+			radData.innerText = deltager[1].sluttTid;
+			nyrad.appendChild(radData);
+			
+			this.#listeTableBody.appendChild(nyrad);
+		})
+	}
 }
 	
 const rootelement = document.getElementById("root");
